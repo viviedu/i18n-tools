eed22b14eb8c542edcaa34d371434a0a64abf248
Alysander Stanley <alysanders@vivi.io>
2024-06-16 21:49:49 +1000

    some defaults

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
 /* eslint-disable no-console */
 
 const fs = require('node:fs').promises;
-
 const { SourceFiles, Translations, UploadStorage } = require('@crowdin/crowdin-api-client');
-const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config();
-
-const crowdinFileId = 42;
-// id of the file we have already uploaded to crowdin can be found with
-// curl -X GET "https://api.crowdin.com/api/v2/projects/{{project_id}}/files" -H "Authorization: Bearer $CROWDIN_TOKEN" -H "Content-Type: application/json"
-
-
-const crowdinProjectId = 654680; // the project id can be found in the api tab of the project https://crowdin.com/project/vivi-box/tools/api
-const uploadFilePath = 'src/assets/i18n/en-GB.json';
-const translationsFolder = 'src/assets/i18n/lang';
-const storageFilename = 'en-GB.json';
-const engineId = 443920; // the translation engine on Alysanders account
-
-// Our language files always have the region lang-region but when we
-// ask crowdin to pre-translate they use languages without region in some cases.
-// You can see on the dashboard for locales by looking at the url. For example:
-// client en-US <- crowdin.com/project/vivi-client/en-US
-// client de-DE <- crowdin.com/project/vivi-client/de
-// client pt-PT <- crowdin.com/project/vivi-client/pt-PT
-// or the official api https://developer.crowdin.com/api/v2/#operation/api.languages.getMany has the 'ids'.
-
-const mapCrowdinToClientLocale = {
+const defaultMapCrowdinLocale = {
   de: 'de-DE',
   'pt-PT': 'pt-PT'
 };
 
-
-async function main(token) {
-  const crowdinLocales = Object.keys(mapCrowdinToClientLocale);
+async function pretranslate({
+    token,
+    crowdinFileId,
+    crowdinProjectId = 654680,
+    uploadFilePath = 'src/assets/i18n/en-GB.json',
+    translationsFolder = 'src/assets/i18n/lang',
+    storageFilename,
+    engineId = 443920,
+    mapCrowdinLocale = defaultMapCrowdinLocale
+  }) {
+  const crowdinLocales = Object.keys(mapCrowdinLocale);
 
   const config = {
     token
@@ -42,7 +28,6 @@ async function main(token) {
   const translations = new Translations(config);
   const sourceFiles = new SourceFiles(config);
   const uploadStorage = new UploadStorage(config);
-
   const data = await fs.readFile(uploadFilePath, 'utf8');
 
   console.log(`uploading: ${uploadFilePath}`);
@@ -94,9 +79,9 @@ async function main(token) {
     const fileResponse = await fetch(buildResponse.data.url);
     const file = await fileResponse.text();
 
-    const mappedLocale = mapCrowdinToClientLocale[locale];
-
-    const translatedFilePath = `${translationsFolder}/${mappedLocale}.json`;
+    const mappedLocale = mapCrowdinLocale[locale];
+    const translationExt = path.extname(uploadFilePath); // json or yaml
+    const translatedFilePath = `${translationsFolder}/${mappedLocale}.${translationExt}`;
 
     console.log(`writing: ${translatedFilePath}`);
 
@@ -108,10 +93,4 @@ async function main(token) {
   }
 }
 
-const { CROWDIN_TOKEN } = process.env;
-
-if (!CROWDIN_TOKEN) {
-  throw new Error('CROWDIN_TOKEN not set!');
-} else {
-  main(CROWDIN_TOKEN);
-}
+module.exports = { pretranslate };
