624ffb50a9d96875883def0f5997fa300e877be8
Alysander Stanley <alysanders@vivi.io>
2024-07-17 17:28:29 +1000

    change path back

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const fetch = require('node-fetch');
 // We rewire the import using module alias to use the cjs version of axios which is built as a subfolder in the module.
 // This initialises the rewiring.
 const moduleAlias = require('module-alias');
-moduleAlias.addAlias('axios', '../axios/dist/node/axios.cjs');
+moduleAlias.addAlias('axios', 'node_modules/axios/dist/node/axios.cjs');
 moduleAlias();
-
-const axios = require('axios');
 
 const { SourceFiles, Translations, UploadStorage } = require('@crowdin/crowdin-api-client');
 const path = require('path');
